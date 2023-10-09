ff195c0ad1542c851e4d0562d2219e3f47721ac3
Tenessi <krotov.sergey.017@mail.ru>
2023-10-09 19:33:45 +0600

    initial project

---
+++ resources/js/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        Working!
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/components/TheMain.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        1112233
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
